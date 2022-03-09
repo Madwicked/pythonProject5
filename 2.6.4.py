51768b58e7825731660e9ef2337cf1de66724f3e
Madwicked <spam4apb@gmail.com>
2022-03-09 16:45:12 +0300

    2

--- 2.6.4.py
+++ 2.6.4.py
@@ -1,1 +1,14 @@
-n = int(input)
+a =[]
+b=[]
+n = int(input())
+for i in range ((n ** 2) +1):
+     b.append(i)
+print(b)
+for i in range(n):
+    a.append([0]* n)
+for i in range(n):
+
+    for j in range(n):
+       a[i][j] = b[i +1]
+
+    print(a[i][j], end = '')
